04c35050955c42bc90351b7af5163d0b3608b063
Nazar <nazarshmagel03@gmail.com>
2022-10-31 20:30:21 +0200

    first complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+    return (
+        <nav className='red darken-4'>
+            <div className='nav-wrapper'>
+                <a href='/' className='brand-logo'>
+                    React Shop
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a
+                            href='https://github.com/michey85/react-shop'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Repo
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };

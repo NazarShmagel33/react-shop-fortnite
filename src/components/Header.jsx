d84dc5c730f3f82e982df043e4e0286ef1761938
Nazar <110281611+NazarShmagel33@users.noreply.github.com>
2023-06-14 14:00:28 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,7 @@ function Header() {
                 <a href='/' className='brand-logo'>
                     React Shop
                 </a>
-                <ul id='nav-mobile' className='right hide-on-med-and-down'>
-                    <li>
-                        <a
-                            href='https://github.com/michey85/react-shop'
-                            target='_blank'
-                            rel='noreferrer'
-                        >
-                            Repo
-                        </a>
-                    </li>
-                </ul>
+                
             </div>
         </nav>
     );
